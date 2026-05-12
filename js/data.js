b449f6a3d085716852f2e5ad157481ed78338633
sususu <suguangluan@ourchem.com>
2026-05-12 15:09:50 +0800

    feat: 加了一些

--- js/data.js
+++ js/data.js
@@ -3,5 +3,6 @@
 // 图片放入 images/ 目录，在此处添加新条目即可
 
 const recipes = [
-  { name: '苦瓜炒牛肉', image: 'images/kgcnr.png' }
+  { name: '苦瓜炒牛肉', image: 'images/kgcnr.png' },
+  { name: '广式蒸鲈鱼', image: 'images/gszly.png' }
 ];
--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 
 var currentIndex = 0;
 var recipeListEl = document.getElementById('recipeList');
-var illustrationEl = document.getElementById('illustration');
+var bgLayerEl = document.getElementById('bgLayer');
+var sidebarEl = document.getElementById('sidebar');
+var toggleBtn = document.getElementById('menuToggle');
 
 function renderList() {
   recipeListEl.innerHTML = '';
@@ -22,29 +24,56 @@ function renderList() {
 
 function selectRecipe(index) {
   currentIndex = index;
-  showIllustration(index);
+  showImage(index);
 
   var items = recipeListEl.querySelectorAll('.recipe-item');
   items.forEach(function (item, i) {
     item.classList.toggle('active', i === index);
   });
+
+  hideSidebar();
 }
 
-function showIllustration(index) {
+function showImage(index) {
   var recipe = recipes[index];
-  illustrationEl.classList.remove('fade-in');
-  void illustrationEl.offsetWidth;
+  var img = document.createElement('img');
+  img.src = recipe.image;
+  img.alt = recipe.name;
+  img.className = 'fade-in';
 
-  illustrationEl.innerHTML = '<img src="' + recipe.image + '" alt="' + recipe.name + '">';
-  illustrationEl.classList.add('fade-in');
+  bgLayerEl.innerHTML = '';
+  bgLayerEl.appendChild(img);
 }
+
+function hideSidebar() {
+  sidebarEl.classList.add('hidden');
+  toggleBtn.classList.add('visible');
+}
+
+function showSidebar() {
+  sidebarEl.classList.remove('hidden');
+  toggleBtn.classList.remove('visible');
+}
+
+function toggleSidebar() {
+  if (sidebarEl.classList.contains('hidden')) {
+    showSidebar();
+  } else {
+    hideSidebar();
+  }
+}
+
+toggleBtn.addEventListener('click', toggleSidebar);
+
+// 点击背景图片隐藏列表
+bgLayerEl.addEventListener('click', function () {
+  if (!sidebarEl.classList.contains('hidden')) {
+    hideSidebar();
+  }
+});
 
 function init() {
-  if (recipes.length === 0) {
-    illustrationEl.innerHTML =
-      '<div class="empty-state"><span class="empty-icon">&#127858;</span><p>请在 data.js 中添加菜谱</p></div>';
-    return;
-  }
+  if (recipes.length === 0) return;
   renderList();
   selectRecipe(0);
 }
